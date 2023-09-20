23bcb672af4829c7fdc672a5570a9ac104685361
HENRIQUEZIM <106694756+Henquezim@users.noreply.github.com>
2023-09-20 14:20:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-function tocaSomPom(idElementoAudio){
-document.querySelector(idElementoAudio).play();
+function tocaSomPom(seletorAudio){
+document.querySelector(seletorAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTecla[0].onclick = tocaSomPom;
+//para
 for (let contador = 0;  contador < listaDeTeclas.length; contador++) {
+  
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
   const idAudio = '#som_${intrumento}'; // template string
 
+tecla.onclink = function () {
+  tocaSom(idAudio);
+}
 
-tecla 
+}
